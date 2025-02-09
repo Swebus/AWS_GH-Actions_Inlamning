572e665d9b9f183cb3245303f07fe1dfbbaa84a3
Sebastian Lindén <sebastian.linden@learnet.se>
2025-02-09 04:14:25 +0100

    Final touches

--- Application/Program.cs
+++ Application/Program.cs
@@ -4,8 +4,8 @@ using string_crypter;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "To encrypt a string is the following endpoint: ?input=<stringtoencrypt> \n\n" +
-"To decrypt a string is the following endpoint: ?input=<stringtodecrypt>");
+app.MapGet("/", () => "To encrypt a string use the following endpoint: /Encrypt?input=<stringtoencrypt> \n\n" +
+"To decrypt a string use the following endpoint: /Decrypt?input=<stringtodecrypt>");
 
 app.MapGet("/Encrypt", (string input) => Encrypter.EncryptString(input));
 
